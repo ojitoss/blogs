f0178f09010c26d1f49da658c790fe745fd6739b
Jasil Gonzalez <88213643+ojitoss@users.noreply.github.com>
2026-08-04 15:03:02 -0700

    remove uneccessary '-e' flag

--- scripts/getter.sh
+++ scripts/getter.sh
@@ -1,17 +1,17 @@
 case $1 in
-  0) 
-     echo -e "things-i-regretted-about-autag\n"
-     echo -e "Why is this variable?\n"
-     echo -e "Insane error management\n"
-     echo -e "Why is this variable?\n"
-     echo -e "Over-engineering\n"
-     echo -e "if/else management\n"
-     echo -e "DRY master\n"
-     echo -e "This breaks the project\n"
-     echo -e "Version controlling\n"
-     echo -e "Perfect english\n"
-     echo -e "Obssesed for some reason\n"
-     echo -e "Logo\n"
+  0)
+     echo "things-i-regretted-about-autag"
+     echo "Why is this variable?"
+     echo "Insane error management"
+     echo "Why is this variable?"
+     echo "Over-engineering"
+     echo "if/else management"
+     echo "DRY master"
+     echo "This breaks the project"
+     echo "Version controlling"
+     echo "Perfect english"
+     echo "Obssesed for some reason"
+     echo "Logo"
      ;;
   *) echo None;;
 esac
