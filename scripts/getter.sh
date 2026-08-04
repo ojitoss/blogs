ce80f4c3895f0da9f379a2b892ff4b1afc7fd5cb
Jasil Gonzalez <88213643+ojitoss@users.noreply.github.com>
2026-08-04 14:53:57 -0700

    feat(scripts): add getter

---
+++ scripts/getter.sh
@@ -0,0 +1,17 @@
+case $1 in
+  0) 
+     echo -e "things-i-regretted-about-autag\n"
+     echo -e "Why is this variable?\n"
+     echo -e "Insane error management\n"
+     echo -e "Why is this variable?\n"
+     echo -e "Over-engineering\n"
+     echo -e "if/else management\n"
+     echo -e "DRY master\n"
+     echo -e "This breaks the project\n"
+     echo -e "Version controlling\n"
+     echo -e "Perfect english\n"
+     echo -e "Obssesed for some reason\n"
+     echo -e "Logo\n"
+     ;;
+  *) echo None;;
+esac
